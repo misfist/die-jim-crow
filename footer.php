9c878af899ff1af19ba0e5b7c54dbd0459241925
Pea Lutz <pea@glocal.coop>
2016-05-06 22:01:50 -0400

    New parent theme based on _s

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Die_Jim_Crow
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'die-jim-crow' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'die-jim-crow' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'die-jim-crow' ), 'die-jim-crow', '<a href="http://misfist.com" rel="designer">Pea</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ inc/setup.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Enqueue Styles and Scripts
+ *
+ * @package Die_Jim_Crow
+ */
+
+/**
+ * Set-up Functions for the Child Theme
+ * @link https://codex.wordpress.org/Child_Themes
+ *
+ */
+function djc_setup() {
+    // Register social menu, in case we want one
+    register_nav_menus( array(
+        'social' => __( 'Social Menu', 'die-jim-crow' ),
+    ) );
+}
+add_action( 'after_setup_theme', 'djc_setup' );
+
+/**
+ * Register widgetized areas
+ * @link https://codex.wordpress.org/Function_Reference/register_widget
+ */
+function djc_widgets_init() {
+    register_sidebar( array(
+        'name'          => __( 'Content Footer', 'die-jim-crow' ),
+        'id'            => 'sidebar-content-footer',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h1 class="widget-title">',
+        'after_title'   => '</h1>',
+    ) );
+}
+
+add_action( 'widgets_init', 'djc_widgets_init' );
+
+?>
