b443ae58ae37671a220606dfb25f27f0ab7b070c
Pea Lutz <pea@glocal.coop>
2016-05-07 18:30:49 -0400

    Added custom logo support

--- header.php
+++ header.php
@@ -26,6 +26,9 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
+
+			<?php die_jim_crow_the_custom_logo(); ?>
+			
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
