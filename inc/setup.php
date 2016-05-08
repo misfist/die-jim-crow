17b0d2c5b00f6dfe08f8163ff2d13b0c75429bab
Pea Lutz <pea@glocal.coop>
2016-05-08 00:43:14 -0400

    Added a homepage sidebar.

--- inc/setup.php
+++ inc/setup.php
@@ -98,7 +98,7 @@ add_action( 'after_setup_theme', 'die_jim_crow_setup' );
  * @global int $content_width
  */
 function die_jim_crow_content_width() {
-    $GLOBALS['content_width'] = apply_filters( 'die_jim_crow_content_width', 640 );
+    $GLOBALS['content_width'] = apply_filters( 'die_jim_crow_content_width', 930 );
 }
 add_action( 'after_setup_theme', 'die_jim_crow_content_width', 0 );
 
@@ -118,12 +118,20 @@ function die_jim_crow_widgets_init() {
         'after_title'   => '</h2>',
     ) );
     register_sidebar( array(
+        'name'          => __( 'Home Sidebar', 'die-jim-crow' ),
+        'id'            => 'sidebar-home',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ) );
+    register_sidebar( array(
         'name'          => __( 'Content Footer', 'die-jim-crow' ),
         'id'            => 'sidebar-content-footer',
         'before_widget' => '<aside id="%1$s" class="widget %2$s">',
         'after_widget'  => '</aside>',
-        'before_title'  => '<h1 class="widget-title">',
-        'after_title'   => '</h1>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
     ) );
 }
 add_action( 'widgets_init', 'die_jim_crow_widgets_init' );
--- sidebar.php
+++ sidebar.php
@@ -7,11 +7,23 @@
  * @package Die_Jim_Crow
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
-?>
+if( is_home() && is_active_sidebar( 'sidebar-home' ) ) : ?>
 
-<aside id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</aside><!-- #secondary -->
+    <aside id="secondary" class="widget-area" role="complementary">
+        <?php dynamic_sidebar( 'sidebar-home' ); ?>
+    </aside><!-- #secondary -->
+
+<?php else : ?>
+    <?php
+    if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+        return;
+    }
+    ?>
+    
+    <aside id="secondary" class="widget-area" role="complementary">
+        <?php dynamic_sidebar( 'sidebar-1' ); ?>
+    </aside><!-- #secondary -->
+
+<?php endif; ?>
+
+
