1d855a7468e97323a751be6322acae41e68d0fc0
Pea Lutz <pea@glocal.coop>
2016-05-23 19:22:36 -0400

    Changed bio image to inline rather than background image.

--- template-parts/content-bio.php
+++ template-parts/content-bio.php
@@ -9,6 +9,8 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> style="background-image: url(<?php echo has_post_thumbnail( $id ) ? wp_get_attachment_image_url( get_post_thumbnail_id( $id ), 'photo_album' ) : ''; ?>);">
-	<?php the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' ); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
+    <?php if ( has_post_thumbnail() ) {
+        the_post_thumbnail( 'bio-thumbnail' );
+    }  ?>
 </article><!-- #post-## -->
