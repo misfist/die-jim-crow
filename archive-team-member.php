ca57a51c7a63532f7991935328f5848b79051eb7
Pea Lutz <pea@glocal.coop>
2016-05-23 21:28:58 -0400

    Created separate partial for production bios and other bios.

--- archive-team-member.php
+++ archive-team-member.php
@@ -59,7 +59,12 @@ get_header(); ?>
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', 'bio' );
+
+				if( 'production' == $term->slug ) :
+					get_template_part( 'template-parts/content', 'bio-production' );
+				else :
+					get_template_part( 'template-parts/content', 'bio' );
+				endif;
 
 			endwhile; ?>
 
