bbf3948ed9c590e4f933f4d98e11f32ee52ea488
Pea <pea@misfist.com>
2018-06-18 13:46:57 -0400

    Removed breadcrumb and result count from product listing

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -29,3 +29,23 @@ function die_jim_crow_add_woocommerce_support() {
 	// add_theme_support( 'wc-product-gallery-slider' );
 }
 add_action( 'after_setup_theme', 'die_jim_crow_add_woocommerce_support' );
+
+/**
+ * Remove the breadcrumbs
+ *
+ * @link https://docs.woocommerce.com/document/customise-the-woocommerce-breadcrumb/
+ *
+ * @return void
+ */
+function die_jim_crow_remove_wc_breadcrumbs() {
+  remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0 );
+}
+add_action( 'init', 'die_jim_crow_remove_wc_breadcrumbs' );
+
+/**
+ * Remove Result Count
+ *
+ * @link https://wpbeaches.com/remove-default-sort-in-woocommerce-and-showing-all-results-notice/
+ * @see woocommerce_result_count()
+ */
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
